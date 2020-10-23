4777d7baf85e7eb42dd3ce307bc178fe3741e3c5
Le <nguyenngocle2408@gmail.com>
2020-10-24 01:19:19 +0700

    method basic JavaScript

--- B1/main.js
+++ B1/main.js
@@ -1,2 +1,10 @@
     //window.alert("Hello javascript");
-    document.getElementById("date").innerHTML = Date();
+    // document.getElementById("date").innerHTML = Date();
+    // confirm("Please input field!")
+    // prompt("Please confirm the input")
+    setInterval(function () {
+        console.log("Hello", Math.random())
+    }, 9000)
+    setTimeout(function () {
+        prompt("Please fill the input!!!")
+    }, 1000)
