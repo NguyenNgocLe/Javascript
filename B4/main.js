1950f24332748f2a59d266306cd49b530d9729a0
Le <nguyenngocle2408@gmail.com>
2020-10-25 20:29:26 +0700

    Toán tử logical

---
+++ B4/main.js
@@ -0,0 +1,7 @@
+var result = 'A' && 'B' && 'C'
+console.log(result)
+console.log('-----')
+var numberOne = 1
+var numberTwo = 2
+var resultTwo = numberOne < 0 || numberTwo > 0
+console.log(resultTwo)
