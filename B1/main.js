6bc15a63db49400a7980641d9bc605c8f224bb7a
Le <nguyenngocle2408@gmail.com>
2020-10-25 19:42:40 +0700

    Type Function, Object

--- B1/main.js
+++ B1/main.js
@@ -2,9 +2,15 @@
     // document.getElementById("date").innerHTML = Date();
     // confirm("Please input field!")
     // prompt("Please confirm the input")
-    setInterval(function () {
-        console.log("Hello", Math.random())
-    }, 9000)
-    setTimeout(function () {
-        prompt("Please fill the input!!!")
-    }, 1000)
+    // Prefix &&  postfix
+    // setInterval(function () {
+    //     console.log("Hello", Math.random())
+    // }, 9000)
+    // setTimeout(function () {
+    //     prompt("Please fill the input!!!")
+    // }, 1000)
+    var number = 6
+    var output = number++ * 2 - --number * 2
+    // 6 * 2 = 12
+    // 12 - 6 * 2
+    console.log(output)
---
+++ B2/main.js
@@ -0,0 +1,17 @@
+// Type Function
+var functionExample = function() {
+    var a = 5
+    return a
+}
+
+console.log(functionExample)
+// Type Object
+var objectExample =  {
+    name: "Ngoc Le",
+    age: 22,
+    nickName: "Le Ngoc",
+    method: function() {
+        return 9
+    }
+};
+console.log(objectExample.method)
