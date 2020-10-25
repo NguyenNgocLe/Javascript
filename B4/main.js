ec545867c56e36304588d91497c651637e538115
Le <nguyenngocle2408@gmail.com>
2020-10-25 20:37:51 +0700

    Toán tử logical và câu lệnh điều kiện If v2

--- B4/main.js
+++ B4/main.js
@@ -5,3 +5,8 @@ var numberOne = 1
 var numberTwo = 2
 var resultTwo = numberOne < 0 || numberTwo > 0
 console.log(resultTwo)
+console.log('-----')
+// Mặc định lấy giá trị cuối, nếu 1 trong dãy điều kiện
+// bị dính phải 6 kiểu là: null, 0, false, NaN, undefine thì lấy giá trì này
+// ngược lại thì lấy giá trị cuối cùng!
+console.log('A' && 'B' && null && 'D')
